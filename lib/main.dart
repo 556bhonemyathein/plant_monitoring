5e2846ace7bf26d37dc5c356d1451e10ceb119bf
Bhone Myat Hein <556bhonemyathein@gmail.com>
2026-07-02 16:09:39 +0630

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'plant monitoring',
       theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple)),
       home: const HomeScreen(),
     );
