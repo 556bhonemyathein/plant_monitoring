216b08c253999a4375131733062531f2ab138785
Bhone Myat Hein <556bhonemyathein@gmail.com>
2026-07-23 10:02:48 +0630

    update splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/cupertino.dart';
 // import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 // import 'firebase_options.dart';
 import 'screens/home_screen.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  final binding = WidgetsFlutterBinding.ensureInitialized();
+
+  // Hold the native splash screen until startup work below is done.
+  FlutterNativeSplash.preserve(widgetsBinding: binding);
 
   // Load .env file
   await dotenv.load(fileName: '.env');
@@ -14,6 +18,8 @@ void main() async {
   // await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   runApp(const MyApp());
+
+  FlutterNativeSplash.remove();
 }
 
 class MyApp extends StatelessWidget {
